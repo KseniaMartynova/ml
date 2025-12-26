0dcd4ee9bcf13f8af54d7a64fcc7fec9ef1d7ca5
KseniaMartynova <113133074+KseniaMartynova@users.noreply.github.com>
2025-12-26 17:33:27 +0300

    Create correction.py

---
+++ datasets/scripts/correction.py
@@ -0,0 +1,15 @@
+import pandas as pd
+df = pd.read_csv('final_clusters_with_descriptions.csv')
+
+if len(df.columns) == 3:
+    # Переименовываем колонки
+    df.columns = ['term', 'cluster', 'description']
+    df = df[['term', 'description', 'cluster']]
+    print("переименованo")
+else:
+    print("Внимание: не 3 колонки в файле")
+
+output_file = 'semantic_clusters_hierarchical.xlsx'
+df.to_excel(output_file, index=False)
+
+df.to_csv('semantic_clusters_hierarchical.csv', index=False)
