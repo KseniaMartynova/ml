f3b57d80c7321c7170e1d5bb29497116ae0eafed
KseniaMartynova <113133074+KseniaMartynova@users.noreply.github.com>
2025-12-27 22:43:32 +0300

    Update and rename s_matrix.py to s_matrix_i.py

--- datasets/scripts/s_matrix_i.py
+++ datasets/scripts/s_matrix_i.py
@@ -1,14 +1,9 @@
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
-
 X = np.load('X_matrix.npy')
-
 S = cosine_similarity(X)
-print(f"   Матрица S: {S.shape}")
-print(f"   Значения в S: от {S.min():.4f} до {S.max():.4f}")
-
-
 np.save('S_matrix.npy', S)
+
 
 
 
